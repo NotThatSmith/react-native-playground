0c8f28be316c7a27a1934bc678646ae424a47b99
Michael Smith <michael@msmith.nz>
2016-11-01 21:12:44 +1300

    link android date picker with local notifications

--- app/scheduledNotifications/scheduledNotifications.android.js
+++ app/scheduledNotifications/scheduledNotifications.android.js
@@ -15,30 +15,39 @@ export default class ScheduledNotifications extends Component {
 
   render() {
     return (
-      <TouchableHighlight onPress={() => {
-              DatePickerAndroid.open({
-                date: new Date()
-              }).then((dateChoosen)=> {
-                console.log(dateChoosen)
-                //   PushNotification.localNotificationSchedule({
-                //   message: "Hello World",
-                //   date: new Date(Date.now() + (10 * 1000)) // in 10 secs
-                // })
-              }).then(() => {
-                  return TimePickerAndroid.open({
-                    hour: 14,
-                    minute: 0,
-                    is24Hour: false
-                  })
-                }).then((timeChoosen) => {
-                  console.log(timeChoosen)
-                })
-            }
-          }>
+      <TouchableHighlight onPress={this.onPress}>
           <View style={styles.item}>
             <Text style={styles.buttonText}>Local Notifications Scheduled</Text>
           </View>
       </TouchableHighlight>
     )
+  }
+
+  onPress = () => {
+    DatePickerAndroid.open({date: new Date()})
+                     .then(this.onDatePick)
+  }
+
+  onDatePick = ({action, year, month, day}) => {
+    if (action === DatePickerAndroid.dateSetAction) {
+      TimePickerAndroid.open({
+        hour: 0,
+        minute: 0,
+        is24Hour: false
+      })
+      .then(({action, hour, minute}) => {
+        if (action === TimePickerAndroid.timeSetAction) {
+          const dateToNotify = new Date(year, month, day, hour, minute)
+          PushNotification.localNotificationSchedule({
+            message: "Hello from the past",
+            date: dateToNotify
+          })                      
+        } else {
+          console.log('time dismissed')
+        }
+      })
+    } else {
+      console.log('date dismissed')
+    }
   }
 }
